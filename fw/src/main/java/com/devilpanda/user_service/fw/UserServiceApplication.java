3654beba6a86308f5769fbc5114757411b3132cd
Danny <mezenov.201521@gmail.com>
2021-04-25 13:03:28 +0300

    DevOps configuration
    Eureka server integration

--- fw/src/main/java/com/devilpanda/user_service/fw/UserServiceApplication.java
+++ fw/src/main/java/com/devilpanda/user_service/fw/UserServiceApplication.java
@@ -3,10 +3,12 @@ package com.devilpanda.user_service.fw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = "com.devilpanda.user_service")
 @EnableJpaRepositories(basePackages = "com.devilpanda.user_service.adapter.jpa")
 @EntityScan(basePackages = "com.devilpanda.user_service.domain")
